57c63e25cb6ca057d59522fafee5352b0480f20e
Basirat Aderopo Oseni <basirataderopo@gmail.com>
2026-03-28 06:34:44 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import AboutSection from "@/components/AboutSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Testimonials from "@/components/Testimonials";
 import ContactSection from "@/components/ContactSection";
-import ChatWidget from "@/components/ChatWidget";
+import ChatAssistant from "@/components/ChatAssistant";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -19,7 +19,7 @@ const Index = () => {
       <Testimonials />
       <ContactSection />
       <Footer />
-      <ChatWidget />
+      <ChatAssistant />
     </div>
   );
 };
